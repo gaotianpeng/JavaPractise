4d6cd78658fb636f07dbd551698901159668c9c0
gaotianpeng <gtpsydm@gmail.com>
2024-02-22 17:08:36 +0800

    daily practise

---
+++ daily/src/main/java/org/daily/nio/iodemo/FileReadDemo.java
@@ -0,0 +1,33 @@
+package org.daily.nio.iodemo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class FileReadDemo {
+    public static void readFile(String fileName) {
+        File file = new File(fileName);
+
+        try {
+            Reader reader = new FileReader(file);
+            BufferedReader buffered = new BufferedReader(reader);
+            String data = null;
+            while ((data = buffered.readLine()) != null) {
+                System.out.println(data);
+            }
+            buffered.close();
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        readFile("C:\\work\\algo\\lintcode\\MinimumSpanningTree.java");
+    }
+}
