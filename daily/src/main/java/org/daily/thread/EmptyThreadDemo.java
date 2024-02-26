a55692fe14cbe810a807e69c2aa932fe563075ae
gaotianpeng <gtpsydm@gmail.com>
2024-02-26 18:05:53 +0800

    daily practise

---
+++ daily/src/main/java/org/daily/thread/EmptyThreadDemo.java
@@ -0,0 +1,13 @@
+package org.daily.thread;
+
+public class EmptyThreadDemo {
+    public static void main(String[] args) {
+        Thread th = new Thread();
+        System.out.println("thread name: " + th.getName());
+        System.out.println("thread id: " + th.getId());
+        System.out.println("thread state: " + th.getState());
+        System.out.println("thread priority: " + th.getPriority());
+        th.start();
+        System.out.println("thread state: " + th.getState());
+    }
+}
