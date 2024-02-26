6d05803583d14c569392eec1d6809cf856478abf
gaotianpeng <gtpsydm@gmail.com>
2024-02-26 11:34:07 +0800

    daily practise

---
+++ daily/src/test/java/HelloTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class HelloTest {
+    @BeforeAll
+    public final void beforeAll() {
+        System.out.println("before all");
+    }
+    @AfterAll
+    public final void afterAll() {
+        System.out.println("after all");
+    }
+
+    @Test
+    public void sumTest() {
+        System.out.println("sum test");
+        Assertions.assertEquals(1, 1);
+    }
+}
