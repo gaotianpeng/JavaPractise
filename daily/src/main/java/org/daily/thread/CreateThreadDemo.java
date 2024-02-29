6e0c8e67e62aacaf46fb9371e330c411325cb113
gaotianpeng <gtpsydm@gmail.com>
2024-02-29 21:50:58 +0800

    daily practise

---
+++ daily/src/main/java/org/daily/thread/CreateThreadDemo.java
@@ -0,0 +1,9 @@
+package org.daily.thread;
+
+public class CreateThreadDemo {
+    public static final int MAX_TURN = 5;
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
