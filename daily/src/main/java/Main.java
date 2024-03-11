ae6c4cef4eeda621d78ebe6d2fc1d77787387a92
gaotianpeng <gtpsydm@gmail.com>
2024-03-11 17:02:10 +0800

    daily practise

--- daily/src/main/java/Main.java
+++ daily/src/main/java/Main.java
@@ -2,6 +2,7 @@ import sun.misc.Unsafe;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicStampedReference;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
     public static void main(String[] args) {
---
+++ daily/src/test/java/com/javapractise/daily/concurrency/VolatileTest.java
@@ -0,0 +1,46 @@
+package com.javapractise.daily.concurrency;
+
+import com.javapractise.common.utils.Print;
+import com.javapractise.common.utils.ThreadUtils;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+
+public class VolatileTest {
+    private volatile long value;
+
+    @Test
+    public void testAtomicLong() {
+        final int TASK_AMOUNT = 10;
+
+        ExecutorService pool = ThreadUtils.getCpuIntenseTargetThreadPool();
+
+        final int TURNS = 10000;
+        CountDownLatch countDownLatch = new CountDownLatch(TASK_AMOUNT);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < TASK_AMOUNT; ++i) {
+            pool.submit(() -> {
+                try {
+                    for (int j = 0; j < TURNS; j++) {
+                        value++;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        float time = (System.currentTimeMillis() - start) / 1000F;
+        Print.tcfo("run time is:" + time);
+        Print.tcfo("sum is: " + value);
+        Print.tcfo("the diff is: " + (TURNS * TASK_AMOUNT - value));
+    }
+}
