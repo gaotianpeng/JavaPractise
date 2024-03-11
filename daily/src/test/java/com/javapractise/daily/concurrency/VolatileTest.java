e177ce4d5d3f6c386161933c41a7550c927b57bc
gaotianpeng <gtpsydm@gmail.com>
2024-03-11 17:10:32 +0800

    daily practise

--- daily/src/test/java/com/javapractise/daily/concurrency/VolatileTest.java
+++ daily/src/test/java/com/javapractise/daily/concurrency/VolatileTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class VolatileTest {
     private volatile long value;
