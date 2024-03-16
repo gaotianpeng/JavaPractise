3c18fd39c49201f6c761f19fbeb7db3c8aae9fa2
gtp <gtp@gtpdeMacBook-Pro.local>
2024-03-16 23:02:41 +0800

    daily practise

--- daily/src/main/java/Main.java
+++ daily/src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("参数" + (i+1) + ":" + args[i]);
+        }
+        System.out.println("-Xmx" + Runtime.getRuntime().maxMemory()/1024/1024+"M");
+        System.out.println("-Xmx" + Runtime.getRuntime().maxMemory());
     }
 }
 
